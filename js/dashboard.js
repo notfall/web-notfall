9baf0e798ccdca36fe3c0b3f71d3f835dab7fd30
Banhawy-Laptop <the.benhawy@gmail.com>
2017-01-29 01:02:10 -0600

    Added placeholder for google map section in dashboard page

---
+++ js/dashboard.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	navigator.geolocation.getCurrentPosition(function (position){
+                var lat = position.coords.latitude;
+                var long = position.coords.longitude;
+            });
+});
